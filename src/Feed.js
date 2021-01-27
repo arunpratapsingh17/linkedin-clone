2895736db7fdd1f158d2231677b700c0344113f4
arunpratapsingh17 <218apsj@gmail.com>
2021-01-27 16:31:37 +0530

    Input styling done

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import React from "react";
 import CreateIcon from "@material-ui/icons/Create";
+import ImageIcon from "@material-ui/icons/Image";
 import "./Feed.css";
+import InputOption from "./InputOption";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalenderViewDayIcon from "@material-ui/icons/CalendarViewDay";
 const Feed = () => {
   return (
     <div className="feed">
@@ -11,6 +16,16 @@ const Feed = () => {
             <input type="text" />
             <button type="submit">Send</button>
           </form>
+        </div>
+        <div className="feed_inputOptions">
+          <InputOption title="Photo" Icon={ImageIcon} color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalenderViewDayIcon}
+            title="Write Article"
+            color="#7FC15E"
+          />
         </div>
       </div>
     </div>
